dab447a47b6f8cb73b56682fc40fd095f2814e86
dhatri7 <128492942+dhatri7@users.noreply.github.com>
2023-03-21 21:22:35 +0530

    Create RectAreaLocal.java

---
+++ RectAreaLocal.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class RectAreaLocal {
+    public static void main(String args[]) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the length of the rectangle");
+        int l=sc.nextInt();
+        System.out.println("Enter the breadth of the rectangle");
+        int b=sc.nextInt();
+        System.out.println("Area of the rectangle = "+(l*b));
+    }
+}
