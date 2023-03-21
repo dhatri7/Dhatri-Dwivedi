4c0c709404ff5b2d9ab4c2894fd48379ae5d4150
dhatri7 <128492942+dhatri7@users.noreply.github.com>
2023-03-21 21:08:56 +0530

    Create CircleAreaLocal.java

---
+++ CircleAreaLocal.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class CircleAreaLocal {
+    public static void main(String args[]) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the radius of the circle");
+        int r=sc.nextInt();
+        System.out.println("Area of the circle is "+(3.14*r*r));
+    }
+}
